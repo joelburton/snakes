d4de397f5ecd1467dd8bc300ad4494192bf74b9d
Joel Burton <joel@joelburton.com>
2018-05-17 00:17:41 -0700

    Style & comments

--- snake.js
+++ snake.js
@@ -2,13 +2,13 @@
 
 
 const NROWS = 10;         // number of rows
-const NCOLS = 10;         // number of columns
+const NCOLS = 15;         // number of columns
 const NPELLETS = 5;       // number of growth pellets on board
 const DELAY_SECS = 0.75;  // auto-move delay
 
 let gameOver = false;
 
-// coordinates (y-x) of snake, from head-to-tail
+// coordinates (y-x) of snake, from head-to-tail (starts at top left)
 let snake = ["0-2", "0-1", "0-0"]
 
 // coordinates of pellets (order doesn't matter, so a set, not an array)
@@ -28,6 +28,131 @@ function setCellStyle(style, cell) {
 }
 
 
+/* ************************* MAIN GAME FUNCTIONS *************************** */
+
+/** endGame: do end-of-game announcement & cleanup */
+
+function endGame(msg) {
+  gameOver = true;
+  clearAutoMoveTimer()
+  document.removeEventListener("keydown", readKey);
+  alert(msg)
+}
+
+/** moveSnake: move snake in direction
+ * 
+ * - detects crashes against wall/snake
+ * - handles eating pellet (grows snake & adds replacement)
+ * - updates snake cells on board
+ */
+
+function moveSnake(direction) {
+  // get y, x coordinates of head cell
+  let [y, x] = snake[0].split("-").map(Number)
+
+  // get y.x of new head from the direction
+  if (direction === "UP") y -= 1
+  else if (direction === "DOWN") y += 1
+  else if (direction === "LEFT") x -= 1
+  else if (direction === "RIGHT") x += 1
+
+  // check for crash into wall
+  if (x < 0 || x >= NCOLS || y < 0 || y >= NROWS) {
+    return endGame("Game over: crashed into wall")
+  }
+
+  // draw the new head
+  let newHead = y + "-" + x
+  setCellStyle("snake", newHead)
+
+  // if new head collides with snake, end game
+  if (snake.includes(newHead)) {
+    return endGame("Game over: crashed into self")
+  }
+
+  // add new head to start of snake
+  snake.unshift(newHead)
+
+  // handle eating/not eating a pellet
+  if (pellets.delete(newHead)) {
+    // ate pellet: don't delete tail (snake grows), replace pellet
+    addPellet();
+  } else {
+    // didn't eat pellet: pop off tail & remove style from table cell
+    setCellStyle(null, snake.pop())
+  }
+}
+
+/** addPellet: add pellet to board */
+
+function addPellet() {
+  // loop until finds random point not in snake or a current pellet
+  while (true) {
+    const x = Math.floor(Math.random() * NCOLS);
+    const y = Math.floor(Math.random() * NROWS);
+    pt = y + "-" + x
+
+    if (!pellets.has(pt) && !snake.includes(pt)) {
+      pellets.add(pt)
+      setCellStyle("pellet", pt)
+      return;
+    }
+  }
+}
+
+/** readKey: called when the user presses key (enters a move)
+ * 
+ * - reset auto-move timer
+ * - determine new direction from arrow-key entered (don't allow U-turns)
+ * - move snake
+ */
+
+function readKey(evt) {
+  // since the user entered a move, reset the auto-move timer
+  clearAutoMoveTimer()
+  setAutoMoveTimer()
+
+  const key = evt.code;
+
+  // determine new direction: the snake can't U-turn into itself,
+  // so attempts to do so (ex: can't go right if currently going left)
+  if (key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT"
+  else if (key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT"
+  else if (key === "ArrowUp" && direction !== "DOWN") direction = "UP"
+  else if (key === "ArrowDown" && direction !== "UP") direction = "DOWN"
+  else return;
+
+  evt.preventDefault();
+  moveSnake(direction)
+}
+
+
+/* *************************** AUTO-MOVE TIMER ***************************** */
+
+/** makeAutoMove: called auto-move timer: move in current direction */
+
+function makeAutoMove(t) {
+  moveSnake(direction);
+  setAutoMoveTimer()
+}
+
+/** setAutoMoveTimer: set auto-move timer */
+
+function setAutoMoveTimer() {
+  if (!gameOver) {
+    timerId = window.setTimeout(makeAutoMove, DELAY_SECS * 1000)
+  }
+}
+
+/** clearAutoMoveTimer: clear auto-move timer */
+
+function clearAutoMoveTimer() {
+  window.clearTimeout(timerId)
+}
+
+
+/* ***************************** GAME SETUP ******************************** */
+
 /** createBoard: creates board, giving each cell a unique id of "r-c" */
 
 function createBoard() {
@@ -44,109 +169,6 @@ function createBoard() {
   }
 }
 
-
-
-function endGame(msg) {
-  gameOver = true;
-  clearTimer()
-  document.removeEventListener("keydown", readKey);
-  alert(msg)
-}
-
-function moveSnake(direction) {
-  let head = snake[0].split("-")
-  let y = +head[0]
-  let x = +head[1]
-
-  // get y.x of new head from the direction
-  if (direction === "UP") y -= 1
-  else if (direction === "DOWN") y += 1
-  else if (direction === "LEFT") x -= 1
-  else if (direction === "RIGHT") x += 1
-
-  // check for crash into wall
-  if (x < 0 || x === NCOLS || y < 0 || y === NROWS) {
-    return endGame("crashed into wall")
-  }
-
-  // draw the new head
-  let newHead = y + "-" + x
-  setCellStyle("snake", newHead)
-
-  // if new head collides with snake, end game
-  if (snake.includes(newHead)) {
-    return endGame(`crashed into self!`)
-  }
-
-  // add new head to snake
-  snake.unshift(newHead)
-
-  // handle eating/not eating a pellet
-  if (pellets.delete(newHead)) {
-    // ate pellet, so don't delete tail (snake grows), replace pellet
-    addPellet();
-  } else {
-    // didn't eat pellet, so pop off tail and remove style from table cell
-    setCellStyle("", snake.pop())
-  }
-}
-
-/** addPellet: add pellet to board */
-
-function addPellet() {
-  // loop until we find random location that isn't part of snake
-  // or already a pellet
-  while (true) {
-    const x = Math.floor(Math.random() * NCOLS);
-    const y = Math.floor(Math.random() * NROWS);
-    pt = y + "-" + x
-
-    if (!pellets.has(pt) && !snake.includes(pt)) {
-      pellets.add(pt)
-      setCellStyle("pellet", pt)
-      return;
-    }
-  }
-}
-
-/** readKey: called when the user enters a move */
-
-function readKey(evt) {
-  // since the user entered a move, reset the auto-move timer
-  clearTimer()
-  setTimer()
-
-  // determine new direction: the snake can't U-turn into itself,
-  // so attempts to do so (ex: can't go right if currently going left)
-  if (evt.code === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT"
-  if (evt.code === "ArrowRight" && direction !== "LEFT") direction = "RIGHT"
-  if (evt.code === "ArrowUp" && direction !== "DOWN") direction = "UP"
-  if (evt.code === "ArrowDown" && direction !== "UP") direction = "DOWN"
-
-  moveSnake(direction)
-}
-
-/** hitTimer: called when auto-move timer expires: move in current direction */
-
-function hitTimer(t) {
-  if (!gameOver) {
-    moveSnake(direction);
-    setTimer()
-  }
-}
-
-/** setTimer: set auto-move timer */
-
-function setTimer() {
-  timerId = window.setTimeout(hitTimer, DELAY_SECS * 1000)
-}
-
-/** clearTimer: clear auto-move timer */
-
-function clearTimer() {
-  window.clearTimeout(timerId)
-}
-
 /** setup: setup and start the game */
 
 function setup() {
@@ -157,12 +179,14 @@ function setup() {
     setCellStyle("snake", pt)
   }
 
+  // add initial pellets to board
   for (let i = 0; i < NPELLETS; i++) {
     addPellet();
   }
 
+  // start game: listen for key-moves, and start timer for auto-moves
   document.addEventListener("keydown", readKey);
-  setTimer()
+  setAutoMoveTimer()
 }
 
 setup()
